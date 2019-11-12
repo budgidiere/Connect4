ead4110fe225644312adcc37630c5319005db846
PineapplePirate <jonathan2001@bellsouth.net>
2019-11-12 15:15:14 -0600

    Add code for rules and beginning

--- connect4.py
+++ connect4.py
@@ -1,7 +1,16 @@
-print("Welcome to Connect 4!")
-print("If you are new to Connect 4 (reproduced by Bud and Jonathan) press 1")
-print("If you are familiar with Connect 4 (reproduced by Bud and Jonathan press 2)")
-userinput = input("> ")
+def begining():
+    print("Welcome to Connect 4!")
+    print("If you are new to Connect 4 (reproduced by Bud and Jonathan) press 1")
+    print("If you are familiar with Connect 4 (reproduced by Bud and Jonathan press 2)")
+    userinput = input("> ")
+    if userinput == "1":
+        print("")
+    elif userinput == "2":
+        print("Transporting to Game Zone")
+    else:
+        begining()
+
+begining()
 
 line0 = []
 line1 = []
