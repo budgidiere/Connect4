79130499c30c9e7ecad4f7d113c1b7c87dcf39c2
Bud <sgidiere@gmail.com>
2019-11-12 15:07:09 -0600

    add 2d array  & connect4()

--- connect4.py
+++ connect4.py
@@ -1,1 +1,16 @@
 print("Welcome to Connect 4!")
+
+
+line0 = []
+line1 = []
+line2 = []
+line3 = []
+line4 = []
+line5 = []
+line6 = []
+line7 = []
+
+array = [line0, line1, line2, line3, line4, line5, line6 ,line7]
+
+def connect4():
+    pass
