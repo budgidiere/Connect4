c3d73a6378bd75855d2660aaf37a1c9ed34d27b6
PineapplePirate <jonathan2001@bellsouth.net>
2019-11-12 15:07:08 -0600

    add print statements

--- connect4.py
+++ connect4.py
@@ -1,1 +1,4 @@
 print("Welcome to Connect 4!")
+print("If you are new to Connect 4 (reproduced by Bud and Jonathan) press 1")
+print("If you are familiar with Connect 4 (reproduced by Bud and Jonathan press 2)")
+userinput = input("> ")
