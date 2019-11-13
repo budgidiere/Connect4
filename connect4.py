d39cb35762f229e200d30fca845963504d4d180f
Bud <sgidiere@gmail.com>
2019-11-13 13:18:06 -0600

    working on input

--- connect4.py
+++ connect4.py
@@ -21,14 +21,49 @@ line5 = [0, 0, 0, 0, 0, 0, 0, 0]
 line6 = [0, 0, 0, 0, 0, 0, 0, 0]
 line7 = [0, 0, 0, 0, 0, 0, 0, 0]
 
-array = [line0, line1, line2, line3, line4, line5, line6 ,line7]
+array = [line7, line6, line5, line4, line3, line2, line1, line0]
 
-def connect4():
+done = False
+
+def user1():
     printboard()
+    done = False
     tmpuser1 = input("User 1 where would you like to go? ")
+    tmpuser1 = int(tmpuser1)
+    for item in array:
+        print(item[tmpuser1])
+        if item[tmpuser1] == 0:
+            #print("here1")
+            pass
+        elif item[tmpuser1] != 0:
+            #print("here2")
+            if item == line7:
+                line6[tmpuser1] == "X"
+            elif item == line6:
+                line5[tmpuser1] == "X"
+            elif item == line5:
+                line4[tmpuser1] == "X"
+            elif item == line4:
+                line3[tmpuser1] == "X"
+            elif item == line3:
+                line2[tmpuser1] == "X"
+            elif item == line2:
+                line1[tmpuser1] == "X"
+            elif item == line1:
+                line0[tmpuser1] == "X"
+            elif item == line0:
+                print("Invalid position try again.")
+            else:
+                user1()
+            done = True
+    if done != True:
+        print("here")
+        line7[tmpuser1] == "X"
+    printboard()
+            
 
 def printboard():
     for item in array:
         print(item)
 
-connect4()
+user1()
