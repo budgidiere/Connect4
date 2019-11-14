7c77d5cdf399ee0fbc72e00012b84f72170edf2e
Bud <sgidiere@gmail.com>
2019-11-14 13:24:55 -0600

    Add horizontal and vertical win condtions

--- connect4.py
+++ connect4.py
@@ -36,6 +36,8 @@ line7 = [" ", " ", " ", " ", " ", " ", " ", " "]
 
 array = [line7, line6, line5, line4, line3, line2, line1, line0]
 
+list8 = [0,1,2,3,4,5,6,7]
+
 done = False
 
 def place(player):
@@ -86,14 +88,50 @@ def place(player):
         return
     printboard()
 def checkwinner():
-    
-def checkHV():
- 
-def fliparray():
+    if checkHV("X", array) == True or checkHV("X", fliparray(array)):
+        return "X"
+    elif checkHV("X", array) == True or checkHV("X", fliparray(array)):
+        return "O"
+    else:
+        return "no"
+
+def checkHV(check, inputarray):
+    for item in inputarray:
+        #print(item)
+        #print("here1")
+        i = 0
+        for char in item:
+            #print(char)
+            #print("here3")
+            if char == check:
+                #print("here4")
+                charlist = []
+                for item2 in item[i:i+4]:
+                    #print("here5")
+                    #print(item2)
+                    charlist.append(item2)
+                #print(charlist)
+                if charlist == [check, check, check, check]:
+                    #print("here6")
+                    return True
+            i = i + 1
+    return False
+def fliparray(oldarray):
+    h0 = []
+    h1 = []
+    h2 = []
+    h3 = []
+    h4 = []
+    h5 = []
+    h6 = []
+    h7 = []
+    harray = [h0, h1, h2, h3, h4, h5 ,h6, h7]
+    for i in list8:
+        for item in oldarray:
+            harray[i].append(item[i])
+    return harray
     
       
-            
->>>>>>> Stashed changes
 def winner(player):
     print("Congratulations, {}, has won.".format(player))
     print("")
@@ -107,10 +145,7 @@ def winner(player):
     print("       --        ")
     print("      ----       ")
     print("    --------     ")
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 
 def printboard():
     print("['1', '2', '3', '4', '5', '6', '7', '8']")
@@ -119,9 +154,11 @@ def printboard():
     print(" ")
 
 while True:
-    checkwinner()
-    if gameover != True:
+    if checkwinner() == "no":
         place("X")
+        
+    if checkwinner() == "no":
         place("O")
     else:
-        winner()
+        winner(checkwinner())
+        exit()
